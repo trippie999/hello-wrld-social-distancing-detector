652c44da0789edd347699e6e24a81d221265b21a
Rasheed <35092018+trippie999@users.noreply.github.com>
2021-11-02 17:04:59 +0800

    Create social_distancing_config.py

---
+++ hellowrld/social_distancing_config.py
@@ -0,0 +1,7 @@
+#base path to YOLO directory
+MODEL_PATH = "yolo-coco"
+#initialize minimum probability to filter weak detections along with
+#the threshold when applying non maxima suppression
+
+MIN_CONF = 0.3
+NMS_THRESH = 0.3
